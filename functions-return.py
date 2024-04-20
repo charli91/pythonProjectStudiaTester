1b210affd7d624ffb065d9cc3fbb54a26f6afeb9
Charli91 <k.kotowoda@gmail.com>
2024-04-20 14:35:41 +0200

    Adding subtraction, multiplication and division functions

--- functions-return.py
+++ functions-return.py
@@ -1,13 +1,37 @@
-def add(a, b):
+def addition(a, b):
     c = a + b
     return c
 
 
-def add2(a, b):
+def addition2(a, b):
     return a + b
 
 
-result = add(10, 20)
-print(result)
+resultOfAddition = addition(10, 20)
+print('addition', resultOfAddition)
 
-print(add2(10, 20))
+print('addition2: ', addition2(10, 20))
+
+
+def subtraction(a, b):
+    return a - b
+
+
+print('subtraction: ', subtraction(10, 20))
+
+
+def multiplication(a, b):
+    return a * b
+
+
+print('multiplication:', multiplication(10, 20))
+
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("Can't divide by zero")
+
+
+print('division: ', division(10, 20))
