b1f64bc8e3e6f8711701a3c4508d774557a40350
Charli91 <k.kotowoda@gmail.com>
2024-04-20 15:56:32 +0200

    Adding possibility for user to choose his own numbers for operations

--- functions-return.py
+++ functions-return.py
@@ -7,31 +7,28 @@ def addition2(a, b):
     return a + b
 
 
-resultOfAddition = addition(10, 20)
-print('addition', resultOfAddition)
-
-print('addition2: ', addition2(10, 20))
-
-
 def subtraction(a, b):
     return a - b
-
-
-print('subtraction: ', subtraction(10, 20))
 
 
 def multiplication(a, b):
     return a * b
 
 
-print('multiplication:', multiplication(10, 20))
-
-
 def division(a, b):
     if b != 0:
         return a / b
     else:
-        print("Can't divide by zero")
+        return "Can't divide by zero"
 
 
-print('division: ', division(10, 20))
+first_number = int(input("Tell me your first number: "))
+second_number = int(input("Tell me your second number: "))
+
+resultOfAddition = addition(first_number, second_number)
+
+print('addition: ', resultOfAddition)
+print('addition2: ', addition2(first_number, second_number))
+print('subtraction: ', subtraction(first_number, second_number))
+print('multiplication:', multiplication(first_number, second_number))
+print('division: ', division(first_number, second_number))
