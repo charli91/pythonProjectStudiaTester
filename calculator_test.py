7bd0368f5ae9d6a21da921500d09fc7cdbcea030
Charli91 <k.kotowoda@gmail.com>
2024-04-21 09:59:29 +0200

    Adding new assertions

--- calculator_test.py
+++ calculator_test.py
@@ -1,10 +1,10 @@
 import unittest
 from calculator import Calculator
 
+
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
-
 
     # kolejność wykonywania testów jest alfabetyczna, więc jeśli ma znaczenie kolejność
     # można dodać cyfry na początku np
@@ -13,22 +13,23 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calculator.addition(10, -2), 8)
         self.assertEqual(self.calculator.addition(10, 0), 10)
 
-
     def test_2_subtract(self):
         self.assertEqual(self.calculator.subtraction(25, 10), 15)
         self.assertEqual(self.calculator.subtraction(-10, -20), 10)
         self.assertEqual(self.calculator.subtraction(24, -10), 34)
         self.assertEqual(self.calculator.subtraction(-5, 10), -15)
 
-
     def test_3_multiply(self):
         self.assertEqual(self.calculator.multiplication(4, 6), 24)
-
+        self.assertEqual(self.calculator.multiplication(10, -2), -20)
+        self.assertEqual(self.calculator.multiplication(10, 0), 0)
+        self.assertEqual(self.calculator.multiplication(-3, -5), 15)
 
     def test_4_divide(self):
         self.assertEqual(self.calculator.division(30, 10), 3)
         self.assertEqual(self.calculator.division(10, 0), "Can't divide by 0")
         self.assertEqual(self.calculator.division(-10, 2), -5)
+
 
 if __name__ == '__main__':
     unittest.main()
