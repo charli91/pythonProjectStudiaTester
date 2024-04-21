6c3a7ac80283e1823772e8fdb866ac713f8e99b2
Charli91 <k.kotowoda@gmail.com>
2024-04-21 13:33:38 +0200

    Changing structure of project into folders

---
+++ book/book.py
@@ -0,0 +1,11 @@
+class Book
+    def __init__(self):
+        pass
+    def get_info(self):
+        pass
+    def change_tite(self):
+        pass
+    def change_author(self):
+        pass
+    def change_year(self):
+        pass
