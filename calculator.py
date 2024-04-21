2de175e364e79d90f57ff0437526a978758b998b
Charli91 <k.kotowoda@gmail.com>
2024-04-21 12:04:06 +0200

    Adding functions for calculator with user interaction

--- calculator.py
+++ calculator.py
@@ -13,7 +13,29 @@ class Calculator:
 
     def division(self, a, b):
         if b == 0:
+            # return "Can't divide by zero"
             raise ValueError("Can't divide by 0")
         else:
             c = a / b
             return c
+
+
+def main():
+    print("Calculator")
+    print("Choose your operation:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")
+
+    choice = int(input("Your choice (1/2/3/4): "))
+
+    number1 = float(input("Enter first number: "))
+    number2 = float(input("Enter second number: "))
+
+    calculator = Calculator()
+
+    if choice == 1:
+        result = calculator.addition(number1, number2)
+        print("Your result: ", result)
+
+
+# ten if powoduje wyłączenie uruchomienia tego pliku przy uruchamianiu plików, które mają go zaimportowanego
+if __name__ == '__main__':
+    main()
