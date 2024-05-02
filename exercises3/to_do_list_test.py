bf5a1ed0781ca27a06de790b9032df5599c434dc
Charli91 <k.kotowoda@gmail.com>
2024-05-02 20:55:43 +0200

    Creating first test for adding new task

--- exercises3/to_do_list_test.py
+++ exercises3/to_do_list_test.py
@@ -1,1 +1,22 @@
 from to_do_list import List
+import unittest
+
+
+class ToDoListTest(unittest.TestCase):
+    def setUp(self):
+        self.list = List()
+
+    def test_1_add_task(self):
+        self.assertIn(self.list.add_task(), self.list.tasks)
+
+    def test_2_remove_task(self):
+        pass
+
+    def test_3_mark_task_as_done(self):
+        pass
+
+    def test_4_get_all_tasks(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
